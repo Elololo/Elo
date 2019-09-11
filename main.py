1c59c413f9bc5e06825cfe08e1760a894caaf0cb
aluno <aluno@myco.lxd>
2019-09-11 18:50:19 +0000

    update

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+	return "Olá"
+
+@app.route("/pagina")
+def index():
+	return render_template("index.html")
+
+@app.route("/test")
+def test():
+	return "<input type='submit' value='intervalo'>"
+
+app.run(debug=True,host="0.0.0.0")
